54f061d47b884dfc4ea8676fa261001ec041fc17
Andrew Chernigovsky <chernigovsky108@gmail.com>
2023-08-15 14:54:44 +0300

    lazy-loading MainSlider Videos

--- source/vendorInit/modules/oprimizations/observers.js
+++ source/vendorInit/modules/oprimizations/observers.js
@@ -1,10 +1,10 @@
 const regexp = /A0/gm;
 
 const objObservers = {
-	mainSliderVideos: {
+	mainSliderImages: {
 		elements: function () {
 			let allMainSlidesImages = document.querySelectorAll(
-				".swiper-main .swiper-wrapper .swiper-slide video"
+				".swiper-main .swiper-wrapper .swiper-slide picture"
 			);
 			return allMainSlidesImages;
 		},
@@ -12,20 +12,16 @@ const objObservers = {
 			entries.forEach((entry) => {
 				const { target, isIntersecting } = entry;
 				if (isIntersecting) {
-					target.classList.add("view");
-					console.log('Главный пересекло');
 					let source = target.querySelectorAll("source");
 					let datasrc = target.getAttribute("data-src");
-					console.log(source, 'SPURCE');
 					if (source.length > 0) {
 						source.forEach((sourceEl) => {
-							let src = sourceEl.getAttribute("src");
+							let src = sourceEl.getAttribute("srcset");
 							let reg = src.match(regexp);
 							if (reg !== null) {
-								console.log('HI');
 								reg = reg.slice(0, 1).toString();
 								sourceEl.setAttribute(
-									"src",
+									"srcset",
 									src.replaceAll(reg, datasrc)
 								);
 							}
@@ -36,11 +32,58 @@ const objObservers = {
 		},
 		options: {
 			root: document.querySelector(".swiper-main"),
-			rootMargin: "0px 50px 0px 0px",
-			threshold: 0.3
+			rootMargin: "0px 200px 0px 0px",
+			threshold: 0.1,
 		},
 		observerInit: function () {
 			let init = new IntersectionObserver(this.callback, this.options);
+			return init;
+		},
+		observerItems: function () {
+			let items = this.elements();
+			items.forEach((image) => {
+				this.observerInit().observe(image);
+			});
+		},
+	},
+	mainSliderVideos: {
+		elements: function () {
+			let allMainSlidesVideos = document.querySelectorAll(
+				".swiper-main .swiper-wrapper .swiper-slide video"
+			);
+			return allMainSlidesVideos;
+		},
+		callback: function (entries) {
+			entries.forEach((entry) => {
+				const { target, isIntersecting } = entry;
+				if (isIntersecting) {
+					let source = target.querySelectorAll("source");
+					target.load();
+
+					source.forEach((sourceEl) => {
+						let src = sourceEl.getAttribute("data-src");
+						sourceEl.setAttribute("src", src);
+						sourceEl.setAttribute("autoplay");
+
+					});
+					target.unobserve();
+					source.forEach((sourceEl) => {
+						// let src = sourceEl.getAttribute("data-src");
+						// let src = sourceEl.getAttribute("src")
+						sourceEl.removeAttribute("autoplay");
+						sourceEl.removeAttribute("src");
+					});
+				}
+			});
+		},
+		options: {
+			root: document.querySelector(".swiper-main"),
+			rootMargin: "0px 0px 0px 0px",
+			threshold: 1,
+		},
+		observerInit: function () {
+			let init = new IntersectionObserver(this.callback, this.options);
+
 			return init;
 		},
 		observerItems: function () {
@@ -90,8 +133,6 @@ const objObservers = {
 			entries.forEach((entry) => {
 				const { target, isIntersecting } = entry;
 				if (isIntersecting) {
-					target.classList.add("view");
-					console.log("пересекло");
 					let source = target.querySelectorAll("source");
 					let datasrc = target.getAttribute("data-src");
 					source.forEach((sourceEl) => {
@@ -119,9 +160,7 @@ const objObservers = {
 		},
 		observerItems: function () {
 			let items = this.elements();
-			console.log(this.options.root);
 			items.forEach((video) => {
-				console.log(video);
 				this.observerInit().observe(video);
 			});
 		},
@@ -137,7 +176,6 @@ const objObservers = {
 			entries.forEach((entry) => {
 				const { target, isIntersecting } = entry;
 				if (isIntersecting) {
-					target.classList.add("view");
 
 					let source = target.querySelectorAll("source");
 					let datasrc = target.getAttribute("data-src");
@@ -170,53 +208,6 @@ const objObservers = {
 			let items = this.elements();
 			items.forEach((image) => {
 				this.observerInit().observe(image);
-			});
-		},
-	},
-	mainSliderImages: {
-		elements: function () {
-			let allMainSlidesVideos = document.querySelectorAll(
-				".swiper-main .swiper-wrapper .swiper-slide picture"
-			);
-			return allMainSlidesVideos;
-		},
-		callback: function (entries) {
-			entries.forEach((entry) => {
-				const { target, isIntersecting } = entry;
-				if (isIntersecting) {
-					target.classList.add("view");
-
-					let source = target.querySelectorAll("source");
-					let datasrc = target.getAttribute("data-src");
-					if (source.length > 0) {
-						source.forEach((sourceEl) => {
-							let src = sourceEl.getAttribute("srcset");
-							let reg = src.match(regexp);
-							if (reg !== null) {
-								reg = reg.slice(0, 1).toString();
-								sourceEl.setAttribute(
-									"srcset",
-									src.replaceAll(reg, datasrc)
-								);
-							}
-						});
-					}
-				}
-			});
-		},
-		options: {
-			root: document.querySelector(".swiper-main"),
-			rootMargin: "0px 200px 0px 0px",
-			threshold: 0.1,
-		},
-		observerInit: function () {
-			let init = new IntersectionObserver(this.callback, this.options);
-			return init;
-		},
-		observerItems: function () {
-			let items = this.elements();
-			items.forEach((video) => {
-				this.observerInit().observe(video);
 			});
 		},
 	},
